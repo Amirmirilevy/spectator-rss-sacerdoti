fa5381bfcc69aff2322526b53c1eecc704aa7b26
Amirmirilevy <amirlevy.d@gmail.com>
2025-06-03 17:35:07 +0100

    Update generate_feed.py

--- generate_feed.py
+++ generate_feed.py
@@ -3,38 +3,47 @@ from bs4 import BeautifulSoup
 import json
 import os
 
-BASE_URL = "https://www.spectator.co.uk/writer/jonathan-sacerdoti/"
 HOST_URL = "https://www.spectator.co.uk"
 
 def fetch_articles():
-    response = requests.get(BASE_URL)
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    titles = soup.select("a.article__title-link")
-    images = soup.select("a.article__image-link img")
-    descriptions = soup.select("p.article__excerpt-text")
-    dates = soup.select("time.archive-entry__timestamp")
-
     articles = []
+    page = 1
 
-    # Limit to the shortest list to avoid IndexError
-    num_articles = min(len(titles), len(images), len(descriptions), len(dates))
-    for i in range(num_articles):
-        title = titles[i].text.strip()
-        link = titles[i]["href"]
-        if not link.startswith("http"):
-            link = HOST_URL + link
-        image = images[i]["src"]
-        description = descriptions[i].text.strip()
-        date = dates[i].get("datetime", dates[i].text.strip())
+    while True:
+        url = f"{HOST_URL}/writer/jonathan-sacerdoti/page/{page}/"
+        response = requests.get(url)
+        if response.status_code != 200:
+            break
 
-        articles.append({
-            "title": title,
-            "link": link,
-            "image": image,
-            "description": description,
-            "date": date
-        })
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        titles = soup.select("a.article__title-link")
+        images = soup.select("a.article__image-link img")
+        descriptions = soup.select("p.article__excerpt-text")
+        dates = soup.select("time.archive-entry__timestamp")
+
+        if not titles:
+            break
+
+        count = min(len(titles), len(images), len(descriptions), len(dates))
+        for i in range(count):
+            title = titles[i].text.strip()
+            link = titles[i]["href"]
+            if not link.startswith("http"):
+                link = HOST_URL + link
+            image = images[i]["src"]
+            description = descriptions[i].text.strip()
+            date = dates[i].get("datetime", dates[i].text.strip())
+
+            articles.append({
+                "title": title,
+                "link": link,
+                "image": image,
+                "description": description,
+                "date": date
+            })
+
+        page += 1
 
     return articles
 
@@ -47,24 +56,23 @@ def write_rss(articles):
     items = ""
     for article in articles:
         items += f"""<item>
-  <title>{article["title"]}</title>
-  <link>{article["link"]}</link>
-  <description><![CDATA[{article["description"]}]]></description>
-  <pubDate>{article["date"]}</pubDate>
+  <title>{article['title']}</title>
+  <link>{article['link']}</link>
+  <description>{article['description']}</description>
+  <pubDate>{article['date']}</pubDate>
 </item>
 """
 
     rss = f"""<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0">
 <channel>
-  <title>Jonathan Sacerdoti - Spectator</title>
-  <link>{BASE_URL}</link>
-  <description>Auto-generated RSS feed of articles</description>
+  <title>Jonathan Sacerdoti at The Spectator</title>
+  <link>{HOST_URL}/writer/jonathan-sacerdoti/</link>
+  <description>Latest articles by Jonathan Sacerdoti from The Spectator</description>
   {items}
 </channel>
 </rss>
 """
-
     out_path = os.path.join(os.getcwd(), "feed.xml")
     with open(out_path, "w", encoding="utf-8") as f:
         f.write(rss)
